4f9feac41aca7ab883d443390b82750b7e0101c6
marius <roscaimarius@gmail.com>
2020-10-23 19:07:10 +0300

    added download button only for first image

--- src/MemeSect.js
+++ src/MemeSect.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import first from "./first.jpg";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 
 class MemeSect extends Component {
   constructor() {
@@ -6,11 +9,12 @@ class MemeSect extends Component {
     this.state = {
       topInput: "",
       bottomInput: "",
-      randomImg: "http://i.imgflip.com/1bij.jpg",
+      randomImg: first,
       allMemes: [],
     };
     this.handlerChange = this.handlerChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.handleDll = this.handleDll.bind(this);
   }
 
   componentDidMount() {
@@ -33,12 +37,29 @@ class MemeSect extends Component {
 
   handleClick(e) {
     e.preventDefault();
-    const randNum = Math.floor(Math.random()*this.state.allMemes.memes.length)
-    const randMeme = this.state.allMemes.memes[randNum].url
+    const randNum = Math.floor(
+      Math.random() * this.state.allMemes.memes.length
+    );
+    const randMeme = this.state.allMemes.memes[randNum].url;
     this.setState({
-        randomImg: randMeme
-    })
-    
+      randomImg: randMeme,
+    });
+  }
+
+  handleDll(e) {
+    e.preventDefault();
+    this.setState({
+      randomImg: first,
+    });
+    const divToPrint = document.getElementById("memeID");
+    setTimeout(() => {
+      html2canvas(divToPrint).then((canvas) => {
+        const imgData = canvas.toDataURL("image/jpg");
+        const pdf = new jsPDF();
+        pdf.addImage(imgData, "JPG", 0, 0);
+        pdf.save("generated-meme.pdf");
+      });
+    }, 500);
   }
 
   render() {
@@ -64,11 +85,14 @@ class MemeSect extends Component {
           <button className="genBtn" onClick={this.handleClick}>
             Generate
           </button>
+          <button className="dllBtn" onClick={this.handleDll}>
+            Dll first img Meme
+          </button>
         </form>
-        <div className="meme">
-          <h2 className='toph2'>{this.state.topInput}</h2>
+        <div className="meme" id="memeID">
+          <h2 className="toph2">{this.state.topInput}</h2>
           <img src={this.state.randomImg} alt="problem?" />
-          <h2 className='bottomh2'>{this.state.bottomInput}</h2>
+          <h2 className="bottomh2">{this.state.bottomInput}</h2>
         </div>
       </div>
     );
